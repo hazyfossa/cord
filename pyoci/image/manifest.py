21e7dec17027f4d7e6f23d34c209b44639844a1b
hazyfossa <hazyfossa@gmail.com>
2025-02-01 00:05:19 +0500

    Merge image.index with image.manifest

--- pyoci/image/manifest.py
+++ pyoci/image/manifest.py
@@ -5,11 +5,11 @@ from typing import TYPE_CHECKING, Literal
 
 from pyoci.base_types import Annotations
 from pyoci.common import UNSET, SimpleJsonMixin, Struct, Unset
+from pyoci.image.descriptor import Descriptor, ManifestDescriptor
 from pyoci.image.well_known import MediaType, OciMediaType
-from pyoci.image.descriptor import Descriptor
 
 
-class ImageManifest(Struct, SimpleJsonMixin):
+class Manifest(Struct, SimpleJsonMixin):
     config: Descriptor
     layers: Sequence[Descriptor]
 
@@ -20,3 +20,15 @@ class ImageManifest(Struct, SimpleJsonMixin):
     if not TYPE_CHECKING:
         schemaVersion: Literal[2] = 2
         mediaType: Literal[OciMediaType.image_manifest] = OciMediaType.image_manifest
+
+
+class Index(Struct, SimpleJsonMixin):
+    manifests: list[ManifestDescriptor]
+
+    artifactType: MediaType | Unset = UNSET
+    subject: Descriptor | Unset = UNSET
+    annotations: dict[str, str] | Unset = UNSET
+
+    if not TYPE_CHECKING:
+        schemaVersion: Literal[2] = 2
+        mediaType: Literal[OciMediaType.image_index] = OciMediaType.image_index
