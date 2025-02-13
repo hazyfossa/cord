94c42f1f0794c9f42054754ee5503d3d93843792
hazyfossa <hazyfossa@gmail.com>
2025-02-13 23:41:06 +0500

    Rewrite well known image annotations as an Enum

--- pyoci/image/well_known.py
+++ pyoci/image/well_known.py
@@ -1,7 +1,9 @@
-from enum import StrEnum
+from enum import Enum
 from typing import Annotated
-from msgspec import Meta, defstruct, field
-from pyoci.common import Struct, Unset, UNSET
+
+from msgspec import Meta, field
+
+from pyoci.common import UNSET, Struct, Unset
 
 MediaType = Annotated[
     str,
@@ -12,7 +14,8 @@ MediaType = Annotated[
 
 
 # fmt: off
-class OciMediaType(StrEnum):
+
+class OciMediaType(Enum):
     content_descriptor = "application/vnd.oci.image.descriptor.v1+json"
     layout =             "application/vnd.oci.image.layout.v1+json"
     image_manifest =     "application/vnd.oci.image.manifest.v1+json"
@@ -23,29 +26,25 @@ class OciMediaType(StrEnum):
     layer =              "application/vnd.oci.image.layer.v1.tar"
     layer_gzip =         "application/vnd.oci.image.layer.v1.tar+gzip"
     layer_zstd =         "application/vnd.oci.image.layer.v1.tar+zstd"
+
+
+class ImageAnnotation(Enum):
+    created =           "org.opencontainers.image.annotation.created"
+    authors =           "org.opencontainers.image.annotation.authors"
+    url =               "org.opencontainers.image.annotation.url"
+    documentation =     "org.opencontainers.image.annotation.documentation"
+    source =            "org.opencontainers.image.annotation.source"
+    version =           "org.opencontainers.image.annotation.version"
+    revision =          "org.opencontainers.image.annotation.revision"
+    vendor =            "org.opencontainers.image.annotation.vendor"
+    licenses =          "org.opencontainers.image.annotation.licenses"
+    ref_name =          "org.opencontainers.image.annotation.ref.name"
+    title =             "org.opencontainers.image.annotation.title"
+    description =       "org.opencontainers.image.annotation.description"
+    base_image_digest = "org.opencontainers.image.annotation.base.digest"
+    base_image_name =   "org.opencontainers.image.annotation.base.name"
+
 # fmt: on
-
-
-def _image_annotation(key: str) -> Unset:
-    return field(name=f"org.opencontainers.image.annotation.{key}", default=UNSET)
-
-
-# TODO: consider using an Enum, not a Struct
-class ImageAnnotations(Struct):
-    created: str | Unset = _image_annotation("created")
-    authors: str | Unset = _image_annotation("authors")
-    url: str | Unset = _image_annotation("url")
-    documentation: str | Unset = _image_annotation("documentation")
-    source: str | Unset = _image_annotation("source")
-    version: str | Unset = _image_annotation("version")
-    revision: str | Unset = _image_annotation("revision")
-    vendor: str | Unset = _image_annotation("vendor")
-    licenses: str | Unset = _image_annotation("licenses")
-    ref_name: str | Unset = _image_annotation("ref.name")
-    title: str | Unset = _image_annotation("title")
-    description: str | Unset = _image_annotation("description")
-    base_image_digest: str | Unset = _image_annotation("base.digest")
-    base_image_name: str | Unset = _image_annotation("base.name")
 
 
 def _runtime_annotation(key: str) -> Unset:
